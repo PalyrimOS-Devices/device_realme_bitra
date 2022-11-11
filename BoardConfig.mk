e53aed32c2961ada710d08b00cd4e8df697eccd7
cristianvaz <cristianvaz81993@gmail.com>
2022-11-11 11:06:20 +0100

    bitra: Rebrand to Realme GT Neo 2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,27 +5,18 @@
 #
 
 # Include the common OEM chipset BoardConfig.
--include device/oneplus/sm8250-common/BoardConfigCommon.mk
+-include device/realme/sm8250-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/kebab
+DEVICE_PATH := device/realme/bitra
 
 # Display
-TARGET_SCREEN_DENSITY := 450
+TARGET_SCREEN_DENSITY := 420
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-
-# Partitions
-ifneq ($(WITH_GMS),true)
-BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 614400000
-BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 92160000
-BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 92160000
-endif
-BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 3753902080
-BOARD_SUPER_PARTITION_SIZE := 7516192768
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.qcom
@@ -37,4 +28,4 @@ SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL := INCLUDE_DIR
 SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL_INCLUDE_DIR := $(DEVICE_PATH)/touch/include
 
 # Include the proprietary files BoardConfig.
--include vendor/oneplus/kebab/BoardConfigVendor.mk
+-include vendor/realme/bitra/BoardConfigVendor.mk
